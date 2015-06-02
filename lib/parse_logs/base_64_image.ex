a79ea4393360dc9fa8d523fe78d073a00490bbdf
Gabe Durazo <gabe@gabedurazo.com>
2015-06-02 11:29:59 -0400

    Add tests for existing functionality

--- lib/parse_logs/base_64_image.ex
+++ lib/parse_logs/base_64_image.ex
@@ -4,5 +4,7 @@ defmodule ParseLogs.Base64Image do
   end
 
   def parse_line(line) do
+    [ts|_] = Regex.run(~r/198 <190>1 (.*) app web.1.*/, line, capture: :all_but_first)
+    ts
   end
 end
--- test/parse_logs_test.exs
+++ test/parse_logs_test.exs
@@ -1,7 +1,21 @@
 defmodule ParseLogsTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Base64Video header" do
+    assert ParseLogs.Base64Video.header == "timestamp"
+  end
+
+  test "Base64Video parse_line" do
+    log_line = "198 <190>1 2015-05-12T17:46:41.072613+00:00 app web.1 - - 17:46:41.072 [info] Joined HaveChannel: 5264b0eea08fc0f6c8c164815c30ddc76259851838813a1e7a9a4050802d7e5b"
+    assert ParseLogs.Base64Video.parse_line(log_line) == "2015-05-12T17:46:41.072613+00:00"
+  end
+
+  test "Base64Image header" do
+    assert ParseLogs.Base64Image.header == "timestamp"
+  end
+
+  test "Base64Image parse_line" do
+    log_line = "198 <190>1 2015-05-12T13:15:11.055146+00:00 app web.1 - - 13:15:11.054 [info] Joined HaveChannel: 82d78987fd1cd530302b81d5643739dd9b22f7f7214f10c771b5327729d88fe9"
+    assert ParseLogs.Base64Image.parse_line(log_line) == "2015-05-12T13:15:11.055146+00:00"
   end
 end
