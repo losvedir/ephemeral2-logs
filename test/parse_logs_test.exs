a82eff8687f000dbc6441c12bafbeeed3f036ef9
Gabe Durazo <gabe@gabedurazo.com>
2015-06-02 14:58:46 -0400

    Use Calendar for datetime parsing

--- test/parse_logs_test.exs
+++ test/parse_logs_test.exs
@@ -7,7 +7,7 @@ defmodule ParseLogsTest do
 
   test "Base64Video parse_line" do
     log_line = "198 <190>1 2015-05-12T17:46:41.072613+00:00 app web.1 - - 17:46:41.072 [info] Joined HaveChannel: 5264b0eea08fc0f6c8c164815c30ddc76259851838813a1e7a9a4050802d7e5b"
-    assert ParseLogs.Base64Video.parse_line(log_line) == "2015-05-12T17:46:41.072613+00:00"
+    assert ParseLogs.Base64Video.parse_line(log_line) == 1431452801
   end
 
   test "Base64Image header" do
